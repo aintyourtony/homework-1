3b56f2c605a175f52522a954cceebbaaca8fd219
aintyourtony <jump_into@30.cm>
2018-07-12 00:12:17 +0300

    add $data

--- weather-homework/weather.php
+++ weather-homework/weather.php
@@ -11,14 +11,22 @@
 
     $apiURL = "{$link}?q={$city}&units={$units}&appid={$apiKey}";
 
-    $get = file_get_contents($apiURL) or exit('Не удалось получить данные о погоде');
-    $json = json_decode($get,true) or exit('Ошибка декодирования json файла');
+    $get = file_get_contents($apiURL);
+    $json = json_decode($get,true);
+
+    function checkData($data) { if (empty($data)) { return 'не удалось получить данные'; }
+
+        return $data;
+
+    }
 
     ?>
+
+
 </head>
 <body>
 <h1>Погода в Москве</h1>
-<p>Температура: <?= $json['main']['temp'];?> </p>
-<p>Осадки: <?= $json['weather'][0]['main'];?> </p>
+<p>Температура: <?= checkData($json['main']['temp']);?> </p>
+<p>Осадки: <?= checkData($json['weather'][0]['main']);?> </p>
 </body>
 </html>
