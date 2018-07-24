0400065e98152e7d21494057e5422fb79c6d15f2
aintyourtony <jump_into@30.cm>
2018-07-24 22:49:10 +0300

    stroki-massivi

--- me/lecture_1_3.php
+++ me/lecture_1_3.php
@@ -7,32 +7,26 @@ $continents = [
     'Eurasian' => ['Alces alces', 'Gulo gulo', 'Tamias'],
     'Australia' => ['Ornithorhynchus anatinus', 'Tachyglossidae', 'Macropus rufus']
 ];
-
+$i=0;
+$z=0;
+$twoWords = [];
+$twoWordsMassive=[];
+$oneWordMassive=[];
 foreach ($continents as $key=>$continent) {
     foreach ($continent as $animals) {
         echo '<pre>';
         $twoWords = strpos($animals, ' ');
             if ($twoWords == true) {
                 $twoWordsMassive[] = $animals;
-
+                $oneWordMassive[] = explode(' ', $animals);
+                $firstWords[]=$oneWordMassive[$i++][0];
+                $secondWords[]=$oneWordMassive[$z++][1];
             }
     }
 }
 
-foreach ($twoWordsMassive as $values) {
-    $oneWordMassive[] = explode(' ', $values);    {
-    }
-
-}
-//print_r($oneWordMassive);
-
-foreach ($oneWordMassive as $arrayKey => $wordsMass) {
-    array_keys($wordsMass);
-    $firstWords[] = $wordsMass[0];
-    $secondWords[] = $wordsMass[1];
-       }
-//print_r($firstWords);
-//print_r($secondWords);
+print_r($firstWords);
+print_r($secondWords);
 
 shuffle($firstWords);
 shuffle($secondWords);
@@ -49,65 +43,3 @@ for ($i=0; $i < count ($massShuffled[0]); $i++) {
 $result = array_search($firstWords,$continents);
 
 var_dump($result);
-
-
-
-
-//foreach ($massShuffled as $outputwords) {
- //   echo $outputwords;
-//}
-//           foreach ($continent as $animals) {
-//                        $space = ' ';
-//                        $twoWords = strpos($animals, $space);
-//
-//                      if ($twoWords == true) {
-//
-//                            $firstWord = stristr($animals, $space, true);
-//                            $secondWord = strpbrk($animals, $space);
-//
-//                            $massiveforShuffleFirstword[] = $firstWord;
-//                            $massiveforShuffleSecondword[] = $secondWord;
-//
-//                          $cont[]=[$key=>$firstWord];
-//
-//                          }
-//
-//                                            }
-//            }
-//
-//
-//
-//echo '<pre>';
-//
-//
-//shuffle($massiveforShuffleFirstword);
-////print_r($massiveforShuffleFirstword);
-//shuffle($massiveforShuffleSecondword);
-////print_r($massiveforShuffleSecondword);
-//$C= array_combine($massiveforShuffleFirstword, $massiveforShuffleSecondword);
-//
-////print_r($C);
-//foreach ($C as $first => $second) {
-//
-//
-//        foreach ($cont as $h2cont => $names) {
-//
-//            echo '<pre>';
-//
-//            $a = array_search($first, $names);
-//
-//            echo '<h2>' . $a . '</h2>';
-//
-//    }
-//
-//    echo key($C) . ' ' . $second . '<br>';
-//    next($C);
-//
-//
-//    }
-//
-//
-//
-//
-//
-?>
