ced57fed6c6f6e1117bfce3b18df5782ff22e368
Tony <jumpinto@mail.ru>
2018-08-22 22:05:50 +0300

    working!

--- me/lecture_1_3.php
+++ me/lecture_1_3.php
@@ -7,36 +7,40 @@ $continents = [
     'Australia' => ['Ornithorhynchus anatinus', 'Tachyglossidae', 'Macropus rufus']
 ];
 echo '<pre>';
-$z=0;
+$i = 0;
+$z = 0;
+$x = 0;
 $firstWords=[];
 $secondWords=[];
 $oneWordMassive=[];
-
-
+$twoWordsMassive = [];
 foreach ($continents as $key => $continent) {
     foreach ($continent as $animals) {
         $oneWordMassive[$key][] = explode(' ', $animals);
     }
 }
-
 foreach ($oneWordMassive as $continent => $firstwords) {
-   foreach ($firstwords as $words) {
-       if (count($words) == 2) {
-           $firstWords[$continent][]=$words[0];
-           $secondWords[]=$words[1];
-           shuffle($secondWords);
-
-       }
-   }
+    foreach ($firstwords as $words) {
+        if (count($words) == 2) {
+            $firstWords[$continent][]=$words[0];
+            $secondWords[]=$words[1];
+            shuffle($secondWords);
+        }
+    }
 }
 
-foreach ($firstWords as $continent => $firstWordAnimal) {
+print_r($firstWords);
+print_r($secondWords);
 
-    echo '<h2>' . $continent . '</h2>';
-
-    foreach ($firstWordAnimal as $names) {
-
-        echo $names . ' ' . $secondWords[$z++] . ',';
-
+foreach ($firstWords as $continent => $value) {
+    foreach ($value as $values) {
+        $twoWordsMassive[$continent][] = $values . ' ' .  $secondWords[$i++];
     }
+}
+
+print_r($twoWordsMassive);
+
+foreach ($twoWordsMassive as $continents => $firstWordAnimal) {
+    echo '<h2>' . $continents . '</h2>';
+    echo implode(',', $firstWordAnimal);
 }
