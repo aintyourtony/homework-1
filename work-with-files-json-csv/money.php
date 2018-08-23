b222270f0fa898889f994d57c9d271f04ddb871b
Tony <jumpinto@mail.ru>
2018-08-23 11:47:55 +0300

    working!

--- work-with-files-json-csv/money.php
+++ work-with-files-json-csv/money.php
@@ -2,40 +2,25 @@
 $sum = [];
 $priceAndName = [];
 $csvName = 'pricelist.csv';
-var_dump($argv);
-
-if ($argv[1] == '--today') {
+if (isset($argv[1]) && isset($argv[2])) {
+    $filename = fopen($csvName, "a", LOCK_EX);
+    $price = implode(' ', array_slice($argv,1,1));
+    $name = implode(' ', array_slice($argv, 2));
+    array_push($priceAndName, $price, $name) ;
+    array_unshift($priceAndName, date('Y-m-d'));
+    fputcsv($filename,$priceAndName, ";");
+    $rowSign = implode(',',$priceAndName);
+    echo "Row added - $rowSign";
+    fclose($filename);
+} elseif ($csvName !== FALSE && $argv[1] == '--today') {
     $csvName = 'pricelist.csv';
     $handle = fopen($csvName, "r");
     while (($fileOpen = fgetcsv($handle, '1000', ';')) !== FALSE) {
-
         if ($fileOpen[0] === date('Y-m-d')) {
-
             $sum[] = $fileOpen[1];
-
         }
     }
-
     echo date('Y-m-d') . ' Spendings ' . array_sum($sum);
-} elseif (isset($argv[1]) && isset($argv[2])) {
-
-    $filename = fopen($csvName, "a", LOCK_EX);
-
-    $price = implode(' ', array_slice($argv,1,1));
-
-    $name = implode(' ', array_slice($argv, 2));
-
-    array_push($priceAndName, $price, $name) ;
-
-    array_unshift($priceAndName, date('Y-m-d'));
-
-    fputcsv($filename,$priceAndName, ";");
-
-    $rowSign = implode(',',$priceAndName);
-
-    echo "Row added - $rowSign";
-
-    fclose($filename);
 } else  {
     echo "Fail! Variables are not correct. Put '--today' or start script with such elements {price} and {item name}";
 }
