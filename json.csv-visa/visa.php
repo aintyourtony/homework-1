6536df3266325416d5ba71d4117b3669ad428d6e
Tony <jumpinto@mail.ru>
2018-08-25 19:13:54 +0300

    improving!

--- json.csv-visa/visa.php
+++ json.csv-visa/visa.php
@@ -1,44 +1,37 @@
 <?php
 $csvName = 'data.csv';
 $handle = fopen($csvName, 'r');
-$file = fgetcsv($handle, 1000);
 $csv = array_map('str_getcsv', file($csvName));
 
-$words = $csv;
 $shortest = -1;
 
 if (isset($argv[1])) {
     $input = $argv[1];
-foreach ($words as $word) {
-    $countryies[] = $word[1];
-}
-        foreach ($countryies as $value) {
-            $lev = levenshtein($input, $value);
+foreach ($csv as $word) {
 
+    $lev = levenshtein($input, $word[1]);
 
             if ($lev == 0) {
-            $closest = $value;
+            $closest = $word[1];
             $shortest = 0;
             break;
         }
 
         if ($lev <= $shortest || $shortest < 0) {
-            $closest = $value;
+            $closest = $word[1];
             $shortest = $lev;
         }
     }
 
 if ($shortest == 0) {
-    foreach ($csv as $rowArray) {
-        if ($rowArray[1] == $argv[1]) {
-            echo $closest . ': ' . $rowArray[4];
+
+        if ($word[1] == $argv[1]) {
+            echo $closest . ': ' . $word[4];
         }
-    }
-} else {
+
+    } else {
     echo "Did you mean?: $closest\n";
-}
-
-
+    }
 
 } else {
     echo 'Please type right country name';
